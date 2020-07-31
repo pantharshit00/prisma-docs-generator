b862f1ea4a62818f999d90f38deb11a707fd7677
Harshit Pant <pantharshit00@gmail.com>
2020-07-31 18:06:53 +0530

    feat: cli :D

---
+++ src/cli.ts
@@ -0,0 +1,120 @@
+#!/usr/bin/env node
+
+import meow from 'meow'
+import kleur from 'kleur'
+import express from 'express'
+import { getSchemaPath, getDMMF, getGenerators } from '@prisma/sdk'
+import { DMMF } from '@prisma/generator-helper'
+import { Server } from 'http'
+import fs from 'fs'
+
+const cli = meow(
+  `
+  Usage
+  $ prisma-docs-generator [command] [flags]
+
+  Options
+    -v Prints out the version number
+
+    ${kleur.bold('serve')}
+      --port -p   Specify the port from which this cli should serve the docs
+    
+`,
+  {
+    flags: {
+      port: {
+        type: 'number',
+        alias: 'p',
+        default: 5858,
+      },
+      version: {
+        alias: 'v',
+      },
+    },
+  },
+)
+class ExpressService {
+  exp: express.Express
+  appInstance: Server | null
+  port: number
+  servePath: string
+
+  constructor(port: number, path: string) {
+    this.port = port
+    this.servePath = path
+    this.exp = express()
+    this.appInstance = null
+  }
+
+  start() {
+    this.exp.use('/', express.static(this.servePath))
+    this.appInstance = this.exp.listen(this.port, () => {
+      console.log(
+        `Prisma Docs Generator started at http://localhost:${this.port}`,
+      )
+    })
+  }
+
+  exit() {
+    if (this.appInstance) {
+      this.appInstance.close()
+    }
+  }
+}
+
+async function execute<T extends meow.AnyFlags>(cli: meow.Result<T>) {
+  const {
+    flags: { port },
+    input,
+  } = cli
+  if (input.length < 1) {
+    console.error(kleur.red('No sub command was specified'))
+    cli.showHelp()
+  }
+
+  const mainSubcommand = input[0]
+
+  switch (mainSubcommand) {
+    case 'serve': {
+      //@ts-ignore
+      const schemaPath = await getSchemaPath()
+      if (!schemaPath) {
+        console.error(kleur.red('Unable to find schema.prisma file'))
+        process.exit(1)
+      }
+      const gens = await getGenerators({ schemaPath: schemaPath })
+      const docsGen = gens.find(
+        (gen) => gen.manifest?.prettyName === 'Prisma Docs Generator',
+      )
+      if (!docsGen) {
+        console.error(
+          kleur.red('Prisma Docs Generator was not specified in the schema'),
+        )
+        process.exit(1)
+      }
+
+      const servePath = docsGen.options?.generator.output
+      if (!servePath) {
+        console.error(
+          kleur.red('Unable to resolve output path for the generator'),
+        )
+        process.exit(1)
+      }
+
+      const server = new ExpressService(port as number, servePath)
+      server.start()
+
+      process.on('SIGTERM', () => {
+        server.exit()
+      })
+
+      break
+    }
+    default: {
+      console.error(kleur.red(`Unknown command ${kleur.bold(mainSubcommand)}`))
+      cli.showHelp()
+    }
+  }
+}
+
+execute(cli)
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,37 @@ import { generatorHandler } from "@prisma/generator-helper";
 import HTMLPrinter from "./printer";
 import transformDMMF from "./generator/transformDMMF";
 import * as fs from "fs";
+import * as path from "path";
 
 generatorHandler({
   onManifest() {
     return {
-      defaultOutput: "./output",
+      defaultOutput: "./docs",
       prettyName: "Prisma Docs Generator",
     };
   },
   async onGenerate(options) {
     const dmmf = transformDMMF(options.dmmf);
     const html = new HTMLPrinter(dmmf);
-    await fs.promises.writeFile("./test.html", html.toHTML());
+    if (options.generator.output) {
+      const styleFile = await fs.promises.readFile(
+        path.join(__dirname, "styles", "main.css")
+      );
+      fs.promises.mkdir(options.generator.output, {
+        recursive: true,
+      });
+      fs.promises.mkdir(path.join(options.generator.output, "styles"), {
+        recursive: true,
+      });
+      await fs.promises.writeFile(
+        path.join(options.generator.output, "index.html"),
+        html.toHTML()
+      );
+
+      await fs.promises.writeFile(
+        path.join(options.generator.output, "styles", "main.css"),
+        styleFile
+      );
+    }
   },
 });
